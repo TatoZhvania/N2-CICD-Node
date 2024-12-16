c173f8730101b41c16e0046f9732e3ef49e7919d
TatoZhvania <tatojvania.03@gmail.com>
2024-12-16 22:53:45 +0400

    test add and ignore node_modules

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
-
-// Define a "Hello World" endpoint
-app.get('/', (req, res) => {
-  res.send('Hello World!!!');
-});
-
-// Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
+
+// Start the server only if not in test mode
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
+
+module.exports = app; // Export the app for testing
